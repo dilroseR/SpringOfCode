344b89209aa37faddae1b409b6aec2953a6a8d6c
Dilrose Reji <70878223+dilroseR@users.noreply.github.com>
2021-03-01 18:12:17 +0530

    Add files via upload

---
+++ Q1/Main.py
@@ -0,0 +1,32 @@
+def maxOccur(s):
+	letters=[]
+	countOfeachLetter=[]
+	answer=0
+	for i in range(0,len(s)):
+		if s[i] not in letters:
+			letters.append(s[i])
+	
+	for i in letters:
+		count=0
+		for j in range(0,len(s)):
+			if i==s[j]:
+				count=count+1
+		countOfeachLetter.append(count)
+
+	maximum=countOfeachLetter[0]
+	for i in range(0,len(countOfeachLetter)):
+		if maximum>countOfeachLetter[i]:
+			continue
+		else:
+			maximum=countOfeachLetter[i]
+			answer=i
+	
+	return letters[answer]
+		
+	
+
+
+print("Enter the string: ")
+s=input()
+answer=maxOccur(s)
+print("The max. occurence of a letter: ",answer)
